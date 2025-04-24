15d94604fb3186f11e75e08f162c62847261faeb
Emil Folino <emilfolino@gmail.com>
2025-04-24 09:46:13 +0200

    Fixed navigation in animation example

--- animation/main.js
+++ animation/main.js
@@ -1,9 +1,11 @@
 import Router from "./router.js";
+import Navigation from "./navigation.js";
 
 import ListComponent from "./list-component.js";
 import DetailComponent from "./detail-component.js";
 
 customElements.define("router-outlet", Router);
+customElements.define("navigation-outlet", Navigation);
 
 customElements.define("list-component", ListComponent);
 customElements.define("detail-component", DetailComponent);
